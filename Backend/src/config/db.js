c1dc02ceea6793441c9c6a89e8e353baa30e42cf
ab369 <ompbabhinav2@gmail.com>
2025-09-19 15:57:03 +0530

    c

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
-const db_url=process.env.DB_URL;
+const db_url='mongodb+srv://ompbabhinav2:U3BrGYnZiKnaUDs3@cluster0.3pz91ie.mongodb.net/tindev';
 //connecting to DB
 const connectDB=async()=>{
     await mongoose.connect(db_url);
